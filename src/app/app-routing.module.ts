9b45f3431bfad987b8aaf35d926593c0bb95bdf0
LucasEscotoDaLuz <lucas.escoto@acad.pucrs.br>
2023-02-07 10:25:50 -0300

    Atualização dos componentes do projeto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
+import { ProjetosComponent } from './componente/projetos/projetos.component';
+import { CurriculoComponent } from './componente/curriculo/curriculo.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'curriculo', component: CurriculoComponent},
+  {path: 'projetos', component: ProjetosComponent},
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componente/rodape/rodape.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { CurriculoComponent } from './componente/curriculo/curriculo.component';
+import { ProjetosComponent } from './componente/projetos/projetos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    InicioComponent,
+    CurriculoComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
