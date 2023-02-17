d79312e0e8e966c34aeacd964858c40127653c77
LucasEscotoDaLuz <lucas.escoto@acad.pucrs.br>
2023-02-17 17:10:46 -0300

    Modificando pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { InicioComponent } from './componente/inicio/inicio.component';
 import { ProjetosComponent } from './componente/projetos/projetos.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { EmailService } from './email.service';
 import { NgImageSliderModule } from 'ng-image-slider';
 import { CarouselComponent } from "./componente/projetos/carousel.component";
 import { CurriculoComponent } from "./componente/curriculo/curriculo.component";
